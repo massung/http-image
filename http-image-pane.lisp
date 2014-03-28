4d1c4a3377a22671f2bb275bcaa787e59896ce3e
Jeffrey Massung <massung@gmail.com>
2014-03-28 10:25:48 -0500

    Caching image downloads. Now checking the Content-Type for the actual mime type information if present. Falls back to extension otherwise.

--- http-image-pane.lisp
+++ http-image-pane.lisp
@@ -18,7 +18,7 @@
 ;;;;
 
 (defpackage :http-image-pane
-  (:use :cl :capi :http)
+  (:use :cl :lw :capi :re :http)
   (:export
    #:http-image-pane
 
@@ -32,19 +32,25 @@
 (in-package :http-image-pane)
 
 (defconstant +image-types+
-  '(("ico"  :ico)
-    ("png"  :png)
-    ("jpg"  :jpg)
-    ("jpeg" :jpg)
-    ("gif"  :gif)
-    ("bmp"  :bmp))
+  '(("x-icon" :ico)
+    ("ico"    :ico)
+    ("png"    :png)
+    ("jpg"    :jpeg)
+    ("jpe"    :jpeg)
+    ("jpeg"   :jpeg)
+    ("gif"    :gif)
+    ("bmp"    :bmp))
   "Known extensions for images.")
 
 (defclass http-image-pane (output-pane)
-  ((url     :initarg :url :reader http-image-pane-url)
-   (image                 :reader http-image-pane-image            :initform nil)
-   (error                 :reader http-image-pane-error            :initform nil)
-   (process               :reader http-image-pane-download-process :initform nil))
+  ((url         :initarg :url         :reader http-image-pane-url)
+   (wait-image  :initarg :wait-image  :reader http-image-pane-wait-image       :initform nil)
+   (error-image :initarg :error-image :reader http-image-pane-error-image      :initform nil)
+   (image                             :reader http-image-pane-image            :initform nil)
+   (process                           :reader http-image-pane-download-process :initform nil)
+
+   ;; cached images from various URLs
+   (cache       :initform nil :allocation :class))
   (:default-initargs
    :background :white
    :draw-with-buffer t
@@ -52,24 +58,46 @@
    :destroy-callback 'destroy-http-image-pane
    :display-callback 'display-http-image-pane))
 
+(defstruct cached-image bytes type)
+
 (defmethod create-http-image-pane ((pane http-image-pane))
   "If the URL has been set, then download it in the background."
-  (when (http-image-pane-url pane)
-    (http-image-pane-refresh pane)))
+  (with-slots (cache url wait-image error-image)
+      pane
+    (unless cache
+      (setf cache (make-hash-table :test #'string=)))
+
+    ;; create the wait-image and error-image if provided
+    (when wait-image
+      (setf wait-image (gp:load-image pane wait-image :cache t)))
+    (when error-image
+      (setf error-image (gp:load-image pane error-image :cache t)))
+
+    ;; try and download
+    (when url
+      (http-image-pane-refresh pane))))
 
 (defmethod destroy-http-image-pane ((pane http-image-pane))
   "Stop downloading the image URL if it's still trying."
-  (with-slots (process image)
+  (with-slots (process image wait-image error-image)
       pane
     (when process
       (mp:process-kill process))
-    (when image
-      (gp:free-image pane image))))
+
+    ;; the image might be the wait or error (don't free twice)
+    (when (and image (not (or (eq image wait-image) (eq image error-image))))
+      (gp:free-image pane image))
+
+    ;; free the wait and error images
+    (when wait-image
+      (gp:free-image pane wait-image))
+    (when error-image
+      (gp:free-image pane error-image))))
 
 (defmethod display-http-image-pane ((pane http-image-pane) &rest bounds)
   "Render the pane for an image being downloaded."
   (declare (ignore bounds))
-  (with-slots (image error)
+  (with-slots (image)
       pane
     (let ((w (gp:port-width pane))
           (h (gp:port-height pane)))
@@ -91,34 +119,61 @@
 
             ;; blit the image
             (gp:draw-image pane image x y :from-width iw :from-height ih :to-width to-w :to-height to-h))
-        (gp:draw-rectangle pane 0 0 w h :filled t :foreground (when error :red))))))
+        (gp:draw-rectangle pane 0 0 w h :filled t)))))
 
 (defmethod (setf http-image-pane-url) (image-url (pane http-image-pane))
   "Change the URL and refresh the pane."
-  (with-slots (url error process)
+  (with-slots (url process)
       pane
     (destroy-http-image-pane pane)
 
     ;; set the new url, clear any error, and the image
-    (setf url image-url
-          error nil
-          process nil)
+    (setf process nil
+          url image-url)
 
     ;; refresh
     (http-image-pane-refresh pane)))
 
 (defmethod http-image-pane-refresh ((pane http-image-pane))
   "Downloads a URL as an image into an image-pane."
-  (with-slots (process url)
+  (with-slots (cache process url image wait-image)
       pane
-    (flet ((download ()
-             (with-response (resp (http-get url))
-               (let* ((bytes (map '(vector (unsigned-byte 8)) #'char-code (response-body resp)))
-                      (ext (pathname-type (pathname (url-path (request-url (response-request resp))))))
-                      (type (second (assoc ext +image-types+ :test #'string-equal))))
-                 (apply-in-pane-process pane #'apply-image-data pane bytes type)))))
-      (setf process (mp:process-run-function "HTTP Image Download" '() #'download)))
-    (gp:invalidate-rectangle pane)))
+    (with-url (url url)
+      (labels ((image-type (resp)
+                 (let ((type (with-headers ((content-type "Content-Type"))
+                                 (response-headers resp)
+                               (when (and content-type (eql (search "image/" content-type) 0))
+                                 (subseq content-type 6))))
+                       (ext (pathname-type (pathname (url-path (request-url (response-request resp)))))))
+
+                   ;; use the Content-Type, if that fails, try the file extension
+                   (second (or (assoc type +image-types+ :test #'string-equal)
+                               (assoc ext +image-types+ :test #'string-equal)))))
+
+               ;; lookup cached image or download it
+               (download ()
+                 (let ((cached-image (hcl:with-hash-table-locked cache (gethash (format-url url) cache))))
+                   (unless cached-image
+
+                     ;; download a new image that isn't in the cache
+                     (with-response (resp (http-get url))
+                       (when-let (type (image-type resp))
+                         (let ((bytes (map '(vector (unsigned-byte 8)) #'char-code (response-body resp))))
+                           (setf cached-image (make-cached-image :bytes bytes :type type))
+
+                           ;; save it in the hash
+                           (hcl:with-hash-table-locked cache
+                             (setf (gethash (format-url url) cache) cached-image))))))
+
+                   ;; create the image only if a cached-image exists or the file downloaded successfully
+                   (apply-in-pane-process pane #'apply-image-data pane cached-image))))
+        (setf process (mp:process-run-function "HTTP Image Download" '() #'download)))
+
+      ;; set the current image to the wait image
+      (setf image wait-image)
+
+      ;; redraw the pane
+      (gp:invalidate-rectangle pane))))
 
 (defmethod http-image-pane-stop ((pane http-image-pane))
   "Stop trying to download the image."
@@ -130,16 +185,18 @@
     ;; reset
     (setf process nil)))
 
-(defmethod apply-image-data ((pane http-image-pane) bytes &optional type)
+(defmethod apply-image-data ((pane http-image-pane) cached-image)
   "Given an array of bytes, load it into the image."
-  (with-slots (image error)
+  (with-slots (image error-image)
       pane
-    (let ((external-image (make-instance 'gp:external-image :data bytes :type type)))
-      (handler-case
-          (progn
-            (sys:atomic-exchange image (gp:convert-external-image pane external-image))
-    
-            ;; refresh the pane
-            (gp:invalidate-rectangle pane))
-        (condition (c)
-          (sys:atomic-exchange error (cons external-image c)))))))
+    (setf image (if (null cached-image)
+                    error-image
+                  (with-slots (bytes type)
+                      cached-image
+                    (let ((ex-image (make-instance 'gp:external-image :data bytes :type type)))
+                      (handler-case
+                          (gp:convert-external-image pane ex-image)
+                        (condition (c) error-image))))))
+
+    ;; redraw with the new image
+    (gp:invalidate-rectangle pane)))
